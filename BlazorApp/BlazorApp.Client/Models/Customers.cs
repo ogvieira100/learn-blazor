417412cbd7143aec6fb7f74336e1037b896ee931
ogvieira <osmargv100@gmail.com>
2025-02-22 13:23:16 -0300

    primeiro commit

---
+++ BlazorApp/BlazorApp.Client/Models/Customers.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp.Client.Models
+{
+    public class Customers
+    {
+        public Customers(string name, string surName)
+        {
+            Name = name;
+            SurName = surName;
+        }
+
+        public  string? Name { get; set; }
+        public  string? SurName { get; set; }
+
+        public Customers()
+        {
+            
+        }
+    }
+}
