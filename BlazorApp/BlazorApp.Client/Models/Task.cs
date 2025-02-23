fa2b7ed69504d9bce9565bfdb8edddf226c1883b
ogvieira <osmargv100@gmail.com>
2025-02-23 18:38:49 -0300

    tasks

---
+++ BlazorApp/BlazorApp.Client/Models/Task.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Models
+{
+
+    public enum TaskStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+    public class Tasks
+    {
+        public TaskStatus TaskStatus { get; set; }
+
+        public required string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? DateUpdated { get; set; }
+
+      
+
+
+        public Tasks()
+        {
+            TaskStatus = TaskStatus.NotStarted;
+            DateCreated = DateTime.Now;
+        }
+
+        public Tasks(string name)
+            : base()
+        {
+            Name = name;
+        }
+    }
+}
