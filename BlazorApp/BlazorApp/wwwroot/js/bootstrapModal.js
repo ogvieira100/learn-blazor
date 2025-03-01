135071ce94fd4b006f688e3252e93a095b2df124
ogvieira <osmargv100@gmail.com>
2025-03-01 11:40:42 -0300

    criação de componente modal

---
+++ BlazorApp/BlazorApp/wwwroot/js/bootstrapModal.js
@@ -0,0 +1,13 @@
+﻿window.bootstrapModal = {
+    show: (id) => {
+        var myModal = new bootstrap.Modal(document.getElementById(id));
+        myModal.show();
+    },
+    hide: (id) => {
+        var modalElement = document.getElementById(id);
+        var modal = bootstrap.Modal.getInstance(modalElement);
+        if (modal) {
+            modal.hide();
+        }
+    }
+};
